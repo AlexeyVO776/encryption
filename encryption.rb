2af04bdd41199c5286445b5fb9ee990025e1002a
AlexeyVO776 <oleshkevich.alexey@gmail.com>
2023-03-07 20:55:21 +0100

    SHA2

--- encryption.rb
+++ encryption.rb
@@ -6,12 +6,15 @@ word = gets.chomp
 puts "Каким способом зашифровать (введите 1 или 2):"
 puts "1. MD5"
 puts "2. SHA1"
+puts "3. SHA2"
 encryption_method = gets.chomp.to_i
 
 if encryption_method == 1
   encryption_result = Digest::MD5.hexdigest "#{word }"
 elsif encryption_method == 2
   encryption_result = Digest::SHA1.hexdigest "#{word }"
+elsif encryption_method == 3
+  encryption_result = Digest::SHA2.hexdigest "#{word }"
 end
 
 puts "Вот что получилось:"
